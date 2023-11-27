5ba8e6d94056b4443b54b2f343a72941ff69c623
Renan Brandão <renanbrandmelo@gmail.com>
2023-11-27 17:39:59 -0300

    Aula 3

--- src/Principal.java
+++ src/Principal.java
@@ -1,4 +1,6 @@
+import br.com.alura.screenmatch.calculos.CalculadoraDeTempo;
 import br.com.alura.screenmatch.modelos.Filme;
+import br.com.alura.screenmatch.modelos.Serie;
 
 public class Principal {
     public static void main(String[] args) {
@@ -6,11 +8,27 @@ public class Principal {
         meuFilme.setNome("Poderoso Chefão");
         meuFilme.setAnoDeLancamento(1970);
         meuFilme.setDuracaoEmMinutos(180);
+        System.out.println("Duração: "+ meuFilme.getDuracaoEmMinutos());
         meuFilme.exibeFichaTecnica();
         meuFilme.avalia(8);
         meuFilme.avalia(5);
         meuFilme.avalia(10);
         System.out.println(meuFilme.mediaAvalicacao());
         System.out.println("total de avaliação:" + meuFilme.getTotalDeAvaliacao());
+
+        Serie minhaSerie = new Serie();
+        minhaSerie.setNome("Lost");
+        minhaSerie.setAnoDeLancamento(2000);
+        minhaSerie.setDuracaoEmMinutos(24);
+        minhaSerie.exibeFichaTecnica();
+        minhaSerie.setTemporadas(10);
+        minhaSerie.setEpisodiosPorTemporada(10);
+        minhaSerie.setMinutosPorEpisodio(50);
+        System.out.println("Duração da serie: "+ minhaSerie.getDuracaoEmMinutos());
+
+        CalculadoraDeTempo calculadora = new CalculadoraDeTempo();
+        calculadora.inclui(meuFilme);
+        calculadora.inclui(minhaSerie);
+        System.out.println(calculadora.getTempoTotal());
     }
 }
--- src/br/com/alura/screenmatch/modelos/Filme.java
+++ src/br/com/alura/screenmatch/modelos/Filme.java
@@ -1,54 +1,13 @@
 package br.com.alura.screenmatch.modelos;
 
-public class Filme {
-    private String nome;
-    private int anoDeLancamento;
-    private boolean incluidoNoPlano;
-    private double somaDasAvaliacoes;
-    private int totalDeAvaliacao;
-    private double duracaoEmMinutos;
+public class Filme extends Titulo {
+private String diretor;
 
-    public void setNome(String nome) {
-        this.nome = nome;
+    public String getDiretor() {
+        return diretor;
     }
 
-    public void setAnoDeLancamento(int anoDeLancamento) {
-        this.anoDeLancamento = anoDeLancamento;
-    }
-
-    public void setDuracaoEmMinutos(double duracaoEmMinutos) {
-        this.duracaoEmMinutos = duracaoEmMinutos;
-    }
-    public void setIncluidoNoPlano(boolean incluidoNoPlano) {
-        this.incluidoNoPlano = incluidoNoPlano;
-    }
-    public String getNome() {
-        return nome;
-    }
-
-    public int getAnoDeLancamento() {
-        return anoDeLancamento;
-    }
-
-    public double getDuracaoEmMinutos() {
-        return duracaoEmMinutos;
-    }
-    public boolean getIncluidoNoPlano() {
-        return this.incluidoNoPlano;
-    }
-    public int getTotalDeAvaliacao(){
-        return totalDeAvaliacao;
-    }
-    public void exibeFichaTecnica(){
-        System.out.println("Nome do filme: "+ nome);
-        System.out.println("Ano de Lançamento: "+ anoDeLancamento);
-    }
-
-    public void avalia(double nota){
-        somaDasAvaliacoes += nota;
-        totalDeAvaliacao++;
-    }
-    public double mediaAvalicacao(){
-        return somaDasAvaliacoes / totalDeAvaliacao;
+    public void setDiretor(String diretor) {
+        this.diretor = diretor;
     }
 }
